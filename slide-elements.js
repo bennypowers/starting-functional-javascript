b2f9c6f92a96c5403fcf34ef14425f91f593084b
Benny Powers <benny.p@blink.gg>
2018-05-02 12:43:32 +0300

    Use unpkg

--- slide-elements.js
+++ slide-elements.js
@@ -1,4 +1,4 @@
-import './node_modules/slidem/slidem-deck.js';
-import './node_modules/slidem/slidem-slide.js';
-import './node_modules/slidem/slidem-polymersummit-slide.js';
-import './node_modules/slidem/slidem-video-slide.js';
+import 'https://unpkg.com/slidem/slidem-deck.js';
+import 'https://unpkg.com/slidem/slidem-slide.js';
+import 'https://unpkg.com/slidem/slidem-polymersummit-slide.js';
+import 'https://unpkg.com/slidem/slidem-video-slide.js';
